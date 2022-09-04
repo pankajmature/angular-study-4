336fa9aea034aab13adb0d61d6b91d5affbb2d32
Pankaj Mature <59148097+pankajmature@users.noreply.github.com>
2022-09-04 23:26:28 +0530

    Some changes

--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -5,6 +5,7 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './course-card.component.html',
   styleUrls: ['./course-card.component.css']
 })
+
 export class CourseCardComponent implements OnInit {
 
   constructor() { }
