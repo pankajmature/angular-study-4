6831298aa07246deb8daa9a831732197e7f1d4db
Pankaj Mature <59148097+pankajmature@users.noreply.github.com>
2022-09-04 23:09:50 +0530

    Some changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CourseCardComponent } from './course-card/course-card.component';
+import { AppHeaderComponent } from './app-header/app-header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CourseCardComponent
+    CourseCardComponent,
+    AppHeaderComponent
   ],
   imports: [
     BrowserModule
