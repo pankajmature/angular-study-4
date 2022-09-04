fd07baa1d4858277e617aef39267a54cc0c12d0f
Pankaj Mature <59148097+pankajmature@users.noreply.github.com>
2022-09-04 21:36:22 +0530

    Some changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { COURSES } from 'src/dbdata';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
+
+  courses = COURSES;
+
   title = 'angular-study-4';
+
 }
--- src/dbdata.ts
+++ src/dbdata.ts
@@ -1,7 +1,6 @@
+import { Course } from "./models/course";
 
-
-export const COURSES: any = [
-
+export const COURSES: Course[] = [
     {
         id: 1,
         description: "Angular Core Deep Dive",
@@ -18,15 +17,14 @@ export const COURSES: any = [
         category: 'BEGINNER',
         lessonsCount: 10
     },
-
     {
         id: 3,
         description: 'NgRx In Depth',
         longDescription: "Learn the modern Ngrx Ecosystem, including Store, Effects, Router Store, Ngrx Entity, Dev Tools and Schematics.",
         iconUrl: 'https://s3-us-west-1.amazonaws.com/angular-university/course-images/angular-ngrx-course.png',
-        category: 'ADVANCED'
+        category: 'ADVANCED',
+        lessonsCount: 3
     },
-
     {
         id: 4,
         description: "Angular for Beginners",
@@ -56,33 +54,35 @@ export const COURSES: any = [
         description: 'Angular Advanced Course',
         longDescription: "Learn Advanced Angular functionality typically used in Library Development. Advanced Components, Directives, Testing, Npm",
         iconUrl: 'https://angular-academy.s3.amazonaws.com/thumbnails/advanced_angular-small-v3.png',
-        category: 'ADVANCED'
+        category: 'ADVANCED',
+        lessonsCount: 13
     },
     {
         id: 8,
         description: 'Complete Typescript Course',
         longDescription: "Complete Guide to Typescript From Scratch: Learn the language in-depth and use it to build a Node REST API.",
         iconUrl: 'https://angular-academy.s3.amazonaws.com/thumbnails/typescript-2-small.png',
-        category: 'BEGINNER'
+        category: 'BEGINNER',
+        lessonsCount: 4
     },
     {
         id: 9,
         description: 'Angular Architecture Course',
         longDescription: "Learn the core RxJs Observable Pattern as well and many other Design Patterns for building Reactive Angular Applications.",
         iconUrl: 'https://s3-us-west-1.amazonaws.com/angular-academy/blog/images/rxjs-reactive-patterns-small.png',
-        category: 'BEGINNER'
+        category: 'BEGINNER',
+        lessonsCount: 6
     },
     {
         id: 10,
         description: "Angular Material Course",
         iconUrl: "https://s3-us-west-1.amazonaws.com/angular-university/course-images/material_design.png",
         longDescription: "Build Applications with the official Angular Widget Library",
-        category: 'ADVANCED'
+        category: 'ADVANCED',
+        lessonsCount: 5
     }
-
 ];
 
-
-export function findCourseById(courseId:number) {
+export function findCourseById(courseId : number) {
     return COURSES.find(course => course.id == courseId);
 }
